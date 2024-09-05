76c067599bd6ab43ea0afd78e1aabfd29d8ec0a8
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-05 21:57:53 +0300

    provide bloc prvider to character screen

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,13 +1,23 @@
+import 'package:breaking_bad_caracters/buisness_logic_layer/cubit/characters_cubit.dart';
+import 'package:breaking_bad_caracters/data_layer/repos/characters_repositry.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'constants/strings.dart';
 import 'presentation_layer/screens/characters_deatailsa_screen.dart';
 import 'presentation_layer/screens/characters_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
+  late CharactersRepositry charactersRepositry;
+  late CharactersCubit charactersCubit;
   Route? generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case characterScreen:
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => CharactersCubit(charactersRepositry),
+                  child: const CharactersScreen(),
+                ));
       case charactersDetails:
         return MaterialPageRoute(
             builder: (_) => const CharactersDeatailsaScreen());
