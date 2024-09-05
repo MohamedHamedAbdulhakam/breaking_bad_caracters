fc25fe1a4b6dee268b2865fe831d65d5e82dcb07
MohamedHamedAbdulhakam <mohamedhamedabdulhakam@gmail.com>
2024-09-05 23:16:00 +0300

    add bloc provider to home screen

--- lib/presentation_layer/screens/characters_screen.dart
+++ lib/presentation_layer/screens/characters_screen.dart
@@ -1,7 +1,24 @@
+import 'package:breaking_bad_caracters/buisness_logic_layer/cubit/characters_cubit.dart';
+import 'package:breaking_bad_caracters/data_layer/models/characters.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
+  //I convert to statefulWidget because we use blocProvider
   const CharactersScreen({super.key});
+
+  @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+  @override
+  void initState() {
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
